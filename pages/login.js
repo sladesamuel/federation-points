57b7c00f05b8e21cd33676d80148ad2d68d2caec
Samuel Slade <samuel.pjm.slade@gmail.com>
2022-04-28 20:58:29 +0100

    Login button with login dialog

--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,21 @@
-const Login = () => <div>TODO: Login</div>;
+import auth from "../utils/auth";
+
+const login = () => {
+  auth.authenticate();
+};
+
+const Login = () => (
+  <>
+    <div>You must login</div>
+    <button
+      onClick={(e) => {
+        e.preventDefault();
+        login();
+      }}
+    >
+      Login
+    </button>
+  </>
+);
 
 export default Login;
